8df1408c3987860794d6bcb77ef0662e4556f137
ALUMNADO\rovipo <vinesroberto13@gmail.com>
2018-10-31 12:11:55 +0100

    Subimos Proyecto 02 - Creacion de Tablero

---
+++ creacionDeTablero.js
@@ -0,0 +1,23 @@
+var fila1=[1,2,3,4,5,6];
+var fila2=[1,2,3,4,5,6];
+var fila3=[1,2,3,4,5,6];
+var fila4=[1,2,3,4,5,6];
+var fila5=[1,2,3,4,5,6];
+var fila6=[1,2,3,4,5,6];
+var color1=["red","pink","blue","yellow","white","green"];
+var color2=["red","pink","blue","yellow","white","green"];
+var color3=["red","pink","blue","yellow","white","green"];
+var color4=["red","pink","blue","yellow","white","green"];
+var color5=["red","pink","blue","yellow","white","green"];
+var color6=["red","pink","blue","yellow","white","green"];
+
+var fila=(fila1,fila2,fila3,fila4,fila5,fila6);
+var color=(color1,color2,color3,color4,color5,color6);
+
+function IniciaTablero(fila,color) {
+}
+IniciaTablero(fila,color)
+
+function CompruebaTablero(fila,color){
+}
+console.log(fila,color);
